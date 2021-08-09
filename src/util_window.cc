d70e7cfb3bfb067e11c6f8c54b9a513b6f8c2a63
huhuang03 <huhuang03@gmail.com>
2021-08-09 19:40:58 +0800

    up

--- src/util_window.cc
+++ src/util_window.cc
@@ -3,8 +3,31 @@
 //
 
 #include "include/util_window.h"
+#include <TlHelp32.h>
+
+static bool enumFindWindow() {
+
+}
 
 HWND eb::findWindow(std::string processName, std::string windowName) {
-    return NULL;
+    auto thSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (!thSnap) {
+        return nullptr;
+    }
+    PROCESSENTRY32 pe;
+
+    if (!Process32Next(thSnap, &pe)) {
+        return nullptr;
+    }
+
+    do {
+        if (processName == pe.szExeFile) {
+            // ok, we find the process
+            // but how can we get the window?
+        }
+    } while (Process32Next(thSnap, &pe));
+
+    CloseHandle(thSnap);
+    return nullptr;
 }
 
