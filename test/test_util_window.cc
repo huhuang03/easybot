b76084d72476d5a34197d1779313f0cd49289400
york <york@duomai.com>
2021-08-09 20:59:10 +0800

    up

--- test/test_util_window.cc
+++ test/test_util_window.cc
@@ -2,11 +2,8 @@
 // Created by huhua on 2021/8/9.
 //
 #include <gtest/gtest.h>
+#include <util_window.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-// Expect two strings not to be equal.
-EXPECT_STRNE("hello", "world");
-// Expect equality.
-EXPECT_EQ(7 * 6, 42);
+TEST(UtilWindow, getProcessId) {
+    eb::findProcessId("")
 }
