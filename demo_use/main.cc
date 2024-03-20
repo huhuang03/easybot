2be8e45c8f2c08b91fd50566761325d12eabeb9e
胡文凡 <wenfan.hu@infaith.com.cn>
2024-03-20 13:23:36 +0800

    fix: update install script

---
+++ demo_use/main.cc
@@ -0,0 +1,3 @@
+//
+// Created by hwf on 2024/3/20.
+//
--- include/private/preload.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by huhua on 2021/12/28.
-//
-
-#ifndef EASYBOT_PRELOAD_H
-#define EASYBOT_PRELOAD_H
-
-#define NS eb
-#endif //EASYBOT_PRELOAD_H
